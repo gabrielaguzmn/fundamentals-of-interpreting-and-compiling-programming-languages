70cf69c11c843d15a19e48d6da46bbeb84f23b66
juan pablo moreno hernandez <juan.moreno.hernandez@correounivalle.edu.co>
2025-02-27 21:38:59 -0500

    create branch and funtions

--- first-workshop.rkt
+++ first-workshop.rkt
@@ -2,9 +2,32 @@
 ;
 ; Gabriela Guzmán 2326772-3743
 ; Valentina Sanchéz COD
-; Juan Pablo Moreno COD
+; Juan Pablo Moreno 2372232-3743
 
 #lang eopl
+
+;append
+(define juntarListas
+  (lambda (l1 l2)
+    (if (null? l1) l2
+        (cons (car l1)
+              (juntarListas (cdr l1) l2)))
+    ))
+
+;lenght
+(define (longi L)
+  (if (null? L)
+      0
+      (+ 1 (longi (cdr L)))))
+
+;filter
+
+
+(define (filtrar x L)
+  (cond
+    [(null? L) '()]   ; Caso base: lista vacía, devuelve lista vacía
+    [(< (car L) x) (cons (car L) (filtrar x (cdr L)))] ; Si el primer elemento es menor que x, lo conserva
+    [else (filtrar x (cdr L))])) ; Si no, lo omite y sigue con el resto
 
 ;  point 2
 
@@ -19,7 +42,7 @@
                   (cond
                    [(null? L) '()] ;si la lista esta vacia, retorna una lista vacia
                    [(zero? n) (cons x (cdr L))] ; si n es 0 reemplaza el primer elemento
-                   [(>= n (length L)) 'error]  ; Si n es mayor o igual a la longitud de L, retorna 'error
+                   [(>= n (longi L)) 'error]  ; Si n es mayor o igual a la longitud de L, retorna 'error
                    [else (cons (car L) (list-set (cdr L) (- n 1) x))]
                    )
                   )
@@ -31,16 +54,25 @@
     [(equal? (car L) E1) (cons E2 (swapper E1 E2 (cdr L)))]  ; Si el primer elemento es E1, lo cambia por E2
     [(equal? (car L) E2) (cons E1 (swapper E1 E2 (cdr L)))]  ; Si el primer elemento es E2, lo cambia por E1
     [else (cons (car L) (swapper E1 E2 (cdr L)))]))  ; Si no es ninguno, lo deja igual y sigue con el resto
+
 ;punto 9
 
-(define (inversions L)
-  (define (count-inversions L rest)
-    (cond
-      [(null? rest) 0]  ; Si no quedan elementos en la lista, no hay más inversiones
-      [else (+ (length (filter (lambda (x) (< x (car L))) (cdr rest))) 
-               (count-inversions (cdr L) (cdr rest)))]))  ; Llamada recursiva
 
-  (count-inversions L L))
+(define (inversions L) 
+  
+  (cond
+  [(equal? (longi(filtrar (car L) L)) 0) (inversions(cdr L))]
+  [(> (longi(filtrar (car L) L)) 1) (inversions (juntarListas ((cdr L) ((longi(filtrar (car L) L))))) )]
+  
+
+  [else (car L)]
+  )  
+   
+  
+  )
+
+
+
   
 ;punto 12
 
@@ -52,12 +84,12 @@
                    filter)))  ; Se mantiene el filtro
 ;punto 15
 
-(define (count-odd-and-even tree)
-  (if (null? tree)
+(define (count-odd-and-even arbol)
+  (if (null? arbol)
       '(0 0)  ; Caso base: árbol vacío, retorna (0,0)
-      (let* ([valor (car tree)]
-             [izq (if (and (pair? (cdr tree)) (pair? (cadr tree))) (cadr tree) '())]
-             [der (if (and (pair? (cdr tree)) (> (length (cdr tree)) 1) (pair? (caddr tree))) (caddr tree) '())]
+      (let* ([valor (car arbol)]
+             [izq (if (and (pair? (cdr arbol)) (pair? (cadr arbol))) (cadr arbol) '())]
+             [der (if (and (pair? (cdr arbol)) (> (longi (cdr arbol)) 1) (pair? (caddr arbol))) (caddr arbol) '())]
              [conteo-izq (count-odd-and-even izq)]
              [conteo-der (count-odd-and-even der)]
              [pares (+ (if (even? valor) 1 0) (car conteo-izq) (car conteo-der))]
@@ -66,19 +98,11 @@
   
 ;punto 18
 
-;append
-(define juntarListas
-  (lambda (l1 l2)
-    (if (null? l1) l2
-        (cons (car l1)
-              (juntarListas (cdr l1) l2)))
-    ))
 
-
-(define (sumar-consecutivos lst)
-  (if (or (null? lst) (null? (cdr lst)))
+(define (sumar-consecutivos L)
+  (if (or (null? L) (null? (cdr L)))
       '()
-      (cons (+ (car lst) (cadr lst)) (sumar-consecutivos (cdr lst)))))
+      (cons (+ (car L) (cadr L)) (sumar-consecutivos (cdr L)))))
 
 (define (pascals-prev-row row)
   (cons 1 (juntarListas (sumar-consecutivos row) '(1))))
