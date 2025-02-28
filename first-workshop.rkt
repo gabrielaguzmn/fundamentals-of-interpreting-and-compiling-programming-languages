1b9da780c3820e1421c1848ef2e06c26c43c3272
valu <valentina.sanchez.castrillon@correounivalle.edu.co>
2025-02-27 20:19:33 -0500

    implementation of all functions with their documentation

--- first-workshop.rkt
+++ first-workshop.rkt
@@ -1,13 +1,13 @@
 ; Taller # 1 - Recursion
 ;
 ; Gabriela Guzmán 2326772-3743
-; Valentina Sanchéz COD
+; Valentina Sanchéz 2324754-3743
 ; Juan Pablo Moreno COD
 
 #lang eopl
 
-;; Punto #1 invert :
 
+;; Punto #1 invert :
 (define invert
   (lambda (l)
     (if (null? l)
@@ -19,14 +19,6 @@
 (display (invert '((a 1) (a 2) (1 b) (2 b)))) (newline)
 (display (invert '((5 9) (10 91) (82 7) (a e) ("hola" "Mundo")))) (newline)
 (display (invert '(("es" "racket") ("genial" "muy") (17 29) (81 o)))) (newline)
-
-;  point 2
-
-(define down
-  (lambda (l)
-    (if (null? l)
-        (list)
-        (cons (list (car l)) (down (cdr l))))))
 
 ;; Punto #4 filter-in :
 
@@ -63,3 +55,69 @@
 (display (cartesian-product '(a b c) '(x y))) (newline)
 (display (cartesian-product '(p q r) '(5 6 7))) (newline)
 (display (cartesian-product '(1 2) '(a b c d))) (newline)
+
+;; Punto 10 up :
+(define up
+  (lambda (L)
+    (if (null? L)
+        '()  
+        (append (unpack (car L))  
+                (up (cdr L))))))  
+
+;; Función auxiliar unpack 
+(define unpack
+  (lambda (x)
+    (if (list? x)
+        x 
+        (list x))))  
+
+;; Pruebas
+(display (up '((1 2) (3 4)))) (newline)  ; prueba del profesor (1 2 3 4) 
+(display (up '((x (y)) z))) (newline)   ; prueba del profesor (x (y) z)
+(display (up '(((a b)) (c d) e))) (newline)  ; mi prueba ((a b) c d e)
+(display (up '((p) q ((r s))))) (newline)  ; mi prueba (p q (r s))
+
+;; Punto 13 operate :
+(define (operate operaciones numeros)
+  (if (null? operaciones) 
+      (car numeros) 
+      (operate (cdr operaciones) 
+               (cons ((car operaciones) (car numeros) (cadr numeros)) 
+                     (cddr numeros)))))
+;; Pruebas
+(display (operate (list + * + - *) '(1 2 8 4 11 6))) (newline)  ; -> 102
+(display (operate (list *) '(4 5))) (newline)  ; -> 20
+(display (operate (list + - * /) '(10 5 2 4 2))) (newline)  ; -> 6
+
+;; Punto 16 Operar-binarias :
+;; Definición de  Funcion auxiliar my-length para contar los elementos de una lista
+(define my-length
+  (lambda (lst)
+    (if (null? lst)  
+        0  
+        (+ 1 (my-length (cdr lst))))))
+
+;; Definición de Operar-binarias
+(define Operar-binarias
+  (lambda (operacionB)
+    (cond
+      ((number? operacionB) operacionB)  
+      ((and (list? operacionB) (= (my-length operacionB) 3))
+       (let ((izq (Operar-binarias (car operacionB)))  
+             (op (cadr operacionB))  
+             (der (Operar-binarias (caddr operacionB))))  
+         (cond
+           ((eqv? op 'suma) (+ izq der))
+           ((eqv? op 'resta) (- izq der))
+           ((eqv? op 'multiplica) (* izq der))
+           (else 'operador-invalido))))  
+      (else 'expresion-invalida))))  
+
+      ;; Pruebas
+(display (Operar-binarias 4)) (newline)  ; -> 4
+(display (Operar-binarias '(2 suma 9))) (newline)  ; -> 11
+(display (Operar-binarias '(2 resta 9))) (newline)  ; -> -7
+(display (Operar-binarias '(2 multiplica 9))) (newline)  ; -> 18
+(display (Operar-binarias '((2 multiplica 3) suma (5 resta 1)))) (newline)  ; -> 10
+(display (Operar-binarias '((2 multiplica (4 suma 1)) multiplica ((2 multiplica 4) resta 1)))) (newline)  ; -> 70
+(display (Operar-binarias '(2 divide 3)))  ; → 'operador-invalido)
