d9d1292d6879f4173431af5cd8cae0d0c1cf0330
juan pablo moreno hernandez <juan.moreno.hernandez@correounivalle.edu.co>
2025-03-01 16:27:36 -0500

    documentation and funtion inversions

--- first-workshop.rkt
+++ first-workshop.rkt
@@ -6,6 +6,16 @@
 
 #lang eopl
 
+
+;; Propósito:
+;; L1 x L2 -> L' : Procedimiento que concatena dos listas l1 y l2 en una sola.
+;;
+;; <lista> := ()
+;;         | (<valor> <lista>)
+;;
+;; Ejemplos:
+;; (juntarListas '(1 2 3) '(4 5 6)) => '(1 2 3 4 5 6)
+;; (juntarListas '() '(a b c)) => '(a b c)
 ;append
 (define juntarListas
   (lambda (l1 l2)
@@ -15,13 +25,29 @@
     ))
 
 ;lenght
+;; Propósito:
+;; L -> Número : Procedimiento que calcula la longitud de una lista dada.
+;;
+;; <lista>  := ()
+;;          | (<elemento> <lista>)
+;; <número> := 0 | 1 | 2 | ...
+;; Ejemplos:
+;; (longi '(a b c d)) => 4
+;; (longi '()) => 0
 (define (longi L)
   (if (null? L)
       0
       (+ 1 (longi (cdr L)))))
 
 ;filter
-
+; Propósito:
+;; Número x L -> L' : Procedimiento que filtra los elementos menores que x en una lista L.
+;;
+;; <lista>  := ()
+;;          | (<número> <lista>)
+;; Ejemplos:
+;; (filtrar 5 '(1 3 7 2 8)) => '(1 3 2)
+;; (filtrar 10 '(15 20 25)) => '()
 
 (define (filtrar x L)
   (cond
@@ -38,16 +64,31 @@
         (cons (list (car l)) (down (cdr l))))))
 
 ;punto 3
+
+;; Propósito:
+;; L x N x X -> L' : Procedimiento que reemplaza el elemento en la posición n de L con x.
+;; <lista>  := ()
+;;          | (<elemento> <lista>)
+;; <número> := 0 | 1 | 2 | ...
+
 (define (list-set L n x)
                   (cond
-                   [(null? L) '()] ;si la lista esta vacia, retorna una lista vacia
+                   [(null? L) '()] 
                    [(zero? n) (cons x (cdr L))] ; si n es 0 reemplaza el primer elemento
                    [(>= n (longi L)) 'error]  ; Si n es mayor o igual a la longitud de L, retorna 'error
                    [else (cons (car L) (list-set (cdr L) (- n 1) x))]
                    )
                   )
+
+
 ;punto 6
 
+
+;; Propósito:
+;; E1 x E2 x L -> L' : Procedimiento que intercambia todas las ocurrencias de E1 con E2 en L.
+;; <lista>  := ()
+;;          | (<elemento> <lista>)
+ 
 (define (swapper E1 E2 L)
   (cond
     [(null? L) '()]  ; Caso base: si la lista está vacía, retorna una lista vacía
@@ -57,24 +98,23 @@
 
 ;punto 9
 
-
-(define (inversions L) 
-  
-  (cond
-  [(equal? (longi(filtrar (car L) L)) 0) (inversions(cdr L))]
-  [(> (longi(filtrar (car L) L)) 1) (inversions (juntarListas ((cdr L) ((longi(filtrar (car L) L))))) )]
-  
-
-  [else (car L)]
-  )  
-   
-  
-  )
+;; Propósito:
+;; L -> Número : Procedimiento que calcula el número de inversiones en una lista.
+;; <lista>  := ()
+;;          | (<elemento> <lista>)
+(define (inversions L)
+  (if (null? L)
+      0
+      (+ (longi (filtrar (car L) (cdr L)))
+         (inversions (cdr L)))))
 
 
 
   
 ;punto 12
+
+;; Propósito:
+;; Número x Número x F x Acum x Predicado -> Número : Procedimiento que acumula valores de a hasta b.
 
 (define (filter-acum a b F acum filter)
   (if (> a b)  ; Caso base: si a supera b, retornar acum
@@ -82,7 +122,13 @@
       (filter-acum (+ a 1) b F  ; Incrementamos a
                    (if (filter a) (F acum a) acum)  ; Aplicamos F si a cumple con filter
                    filter)))  ; Se mantiene el filtro
+
 ;punto 15
+
+;; Propósito:
+;; Árbol -> Lista : Procedimiento que cuenta la cantidad de números pares e impares en un árbol.
+;; <árbol> := ()
+;;         | (<número> <árbol> <árbol>)
 
 (define (count-odd-and-even arbol)
   (if (null? arbol)
@@ -97,7 +143,11 @@
         (list pares impares))))
   
 ;punto 18
-
+;; Propósito:
+;; Número -> Lista : Procedimiento que retorna la fila N del triángulo de Pascal.
+;; <número> := 1 | 2 | 3 | ...
+;; <lista>  := ()
+;;          | (<número> <lista>)
 
 (define (sumar-consecutivos L)
   (if (or (null? L) (null? (cdr L)))
