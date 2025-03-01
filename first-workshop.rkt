e720c22a7fab10893c7aa0549950bcceb9d7c430
Gabriela Guzmán <136855829+gabrielaguzmn@users.noreply.github.com>
2025-02-28 22:42:41 -0500

    add functions 2, 5, 8, 11, 14 and 17

--- first-workshop.rkt
+++ first-workshop.rkt
@@ -6,7 +6,7 @@
 
 #lang eopl
 
-;; PUNTO 1 invert 
+;; Punto 1 invert:
 ;; Propósito:
 ;; L -> L' : Procedimiento que recibe una lista de pares y devuelve una lista con los pares invertidos.
 ;;
@@ -17,13 +17,37 @@
   (lambda (l)
     (if (null? l)
         '()  ; Caso base: lista vacía
-        (cons (list (cadr (car l)) (car (car l))) 
-              (invert (cdr l)))))) 
+        (cons (list (cadr (car l)) (car (car l)))
+              (invert (cdr l))))))
 
 ;; Pruebas
 (display (invert '((a 1) (a 2) (1 b) (2 b)))) (newline)
 (display (invert '((5 9) (10 91) (82 7) (a e) ("hola" "Mundo")))) (newline)
 (display (invert '(("es" "racket") ("genial" "muy") (17 29) (81 o)))) (newline)
+
+;; Punto 2 down:
+;; Propósito:
+;; L -> L' : Procedimiento que recibe una lista y devuelve una lista con cada elemento en una sublista.
+;;
+;; <lista> := ()
+;;         | (<valor> <lista>)
+
+(define down
+  (lambda (l)
+    (if (null? l)
+        (list)
+        (cons (list (car l)) (down (cdr l)
+              )
+            )
+        )
+    )
+  )
+
+;; Pruebas
+(display(down '(1 2 3))) (newline) ; -> ((1) (2) (3))
+(display(down '((una) (buena) (idea)))) (newline) ; -> (((una)) ((buena)) ((idea)))
+(display(down '(un (objeto (mas)) complicado))) (newline) ; -> ((un) ((objeto (mas))) (complicado))
+
 
 ;; Punto #4 filter-in:
 ;; Proposito:
@@ -35,15 +59,47 @@
 (define filter-in
   (lambda (P L)
     (if (null? L)
-        '()  
-        (if (P (car L)) 
-            (cons (car L) (filter-in P (cdr L)))  
-            (filter-in P (cdr L)))))) 
+        '()
+        (if (P (car L))
+            (cons (car L) (filter-in P (cdr L)))
+            (filter-in P (cdr L))))))
 
 ;; Pruebas:
-(display (filter-in number? '(a 2 (1 3) b 7))) (newline)   
-(display (filter-in symbol? '(a (b c) 17 foo))) (newline)  
-(display (filter-in string? '(a b u "univalle" "racket" "flp" 28 90 (1 2 3)))) (newline)  
+(display (filter-in number? '(a 2 (1 3) b 7))) (newline)
+(display (filter-in symbol? '(a (b c) 17 foo))) (newline)
+(display (filter-in string? '(a b u "univalle" "racket" "flp" 28 90 (1 2 3)))) (newline)
+
+;; Punto 5 list-index:
+;; Propósito:
+;; P x L -> num : Procedimiento que devuelve el índice del primer elemento que cumple el predicado P.
+;;
+;; <lista> := ()
+;;          := (<valor> <lista>)
+
+(define list-index
+  (lambda (p l)
+    (letrec
+        (
+         (aux
+          (lambda (p l acc)
+            (cond
+              [(null? l) #f]
+              [(p (car l)) acc]
+              [else (aux p (cdr l) (+ acc 1))]
+              )
+            )
+          )
+         )
+      (aux p l 0)
+      )
+    )
+  )
+
+;; Pruebas
+(display(list-index number? '(a 2 (1 3) b 7))) (newline) ; -> 1
+(display(list-index symbol? '(a (b c) 17 foo))) (newline) ; -> 0
+(display(list-index symbol? '(1 2 (a b) 3))) (newline) ; -> #f
+
 
 ;; Punto #7 cartesian-product:
 ;; Proposito:
@@ -54,10 +110,10 @@
 
 (define cartesian-product
   (lambda (L1 L2)
-    (if (or (null? L1) (null? L2))  
-        '()  
-        (append (pair-with (car L1) L2)  
-                (cartesian-product (cdr L1) L2)))))  
+    (if (or (null? L1) (null? L2))
+        '()
+        (append (pair-with (car L1) L2)
+                (cartesian-product (cdr L1) L2)))))
 
 ;; Función auxiliar pair-with
 ;; Proposito:
@@ -65,13 +121,33 @@
 (define pair-with
   (lambda (x L)
     (if (null? L)
-        '()  
-        (cons (list x (car L)) (pair-with x (cdr L))))))  
+        '()
+        (cons (list x (car L)) (pair-with x (cdr L))))))
 
 ;; Pruebas
 (display (cartesian-product '(a b c) '(x y))) (newline)
 (display (cartesian-product '(p q r) '(5 6 7))) (newline)
 (display (cartesian-product '(1 2) '(a b c d))) (newline)
+
+;; Punto 8 mapping:
+;; Propósito:
+;; (X -> Y) x L1 x L2 -> L' : Procedimiento que recibe una función y dos listas, y devuelve una lista de pares donde la función aplicada al primer elemento de cada par es igual al segundo elemento.
+;;
+;; <lista> := ()
+;;          := (<valor> <lista>)
+
+(define mapping
+  (lambda (f l1 l2)
+    (if (or (null? l1) (null? l2))
+        (list)
+        (if (= (f (car l1)) (car l2))
+            (cons (list (car l1) (car l2)) (mapping f (cdr l1) (cdr l2)))
+            (mapping f (cdr l1) (cdr l2))))))
+
+;; Pruebas
+(display(mapping (lambda (d) (* d 2)) (list 1 2 3) (list 2 4 6))) (newline) ; -> ((1 2) (2 4) (3 6))
+(display(mapping (lambda (d) (* d 3)) (list 1 2 2) (list 2 4 6))) (newline) ; -> ((2 6))
+(display(mapping (lambda (d) (* d 2)) (list 1 2 3) (list 3 9 12))) (newline) ; -> ()
 
 ;; Punto #10 up :
 ;; Proposito:
@@ -82,9 +158,9 @@
 (define up
   (lambda (L)
     (if (null? L)
-        '()  
-        (append (unpack (car L))  
-                (up (cdr L))))))  
+        '()
+        (append (unpack (car L))
+                (up (cdr L))))))
 
 ;; Función auxiliar unpack
 ;; Proposito:
@@ -92,14 +168,32 @@
 (define unpack
   (lambda (x)
     (if (list? x)
-        x 
-        (list x))))  
+        x
+        (list x))))
 
 ;; Pruebas
-(display (up '((1 2) (3 4)))) (newline)  
-(display (up '((x (y)) z))) (newline)   
-(display (up '(((a b)) (c d) e))) (newline)  
-(display (up '((p) q ((r s))))) (newline)  
+(display (up '((1 2) (3 4)))) (newline)
+(display (up '((x (y)) z))) (newline)
+(display (up '(((a b)) (c d) e))) (newline)
+(display (up '((p) q ((r s))))) (newline)
+
+;; Punto 11 zip:
+;; Propósito:
+;; (X x Y -> Z) x L1 x L2 -> L' : Procedimiento que recibe una función y dos listas, y devuelve una lista resultante de aplicar la función a los elementos correspondientes de las dos listas.
+;;
+;; <lista> := ()
+;;          := (<valor> <lista>)
+
+(define zip
+  (lambda (f l1 l2)
+    (if (null? l1)
+        '()
+        (cons (f (car l1) (car l2)) (zip f (cdr l1) (cdr l2))))))
+
+;; Pruebas
+(display (zip + '(1 4) '(6 2))) (newline); -> (7 6)
+(display (zip * '(11 5 6) '(10 9 8))) (newline) ; -> (110 45 48)
+
 
 ;; Punto 13 operate:
 ;; Proposito:
@@ -109,15 +203,35 @@
 ;;          := (<valor> <lista>)
 
 (define (operate operaciones numeros)
-  (if (null? operaciones) 
-      (car numeros) 
-      (operate (cdr operaciones) 
-               (cons ((car operaciones) (car numeros) (cadr numeros)) 
+  (if (null? operaciones)
+      (car numeros)
+      (operate (cdr operaciones)
+               (cons ((car operaciones) (car numeros) (cadr numeros))
                      (cddr numeros)))))
 ;; Pruebas
 (display (operate (list + * + - *) '(1 2 8 4 11 6))) (newline)  ; -> 102
 (display (operate (list *) '(4 5))) (newline)  ; -> 20
 (display (operate (list + - * /) '(10 5 2 4 2))) (newline)  ; -> 6
+
+;; Punto 14 path:
+;; Propósito:
+;; num x BST -> L' : Procedimiento que devuelve el camino para encontrar un número en un árbol binario de búsqueda.
+;;
+;; <BST> := (<valor> <BST> <BST>)
+;;       := ()
+
+(define path
+  (lambda (n bst)
+    (cond
+      [(= (car bst) n) '()]
+      [(> (car bst) n) (cons 'left (path n (cadr bst)))]
+      [else (cons 'right (path n (caddr bst)))]
+      )))
+
+;; Pruebas
+
+(display (path 5 '(10 (5 () ()) (15 () ())))) (newline)  ; -> '(left)
+(display (path 17 '(14 (7 () (12 () ())) (26 (20 (17 () ()) ()) (31 () ()))))) (newline) ; -> (right left left)
 
 ;; Punto #16 Operar-binarias :
 ;; Proposito:
@@ -133,31 +247,52 @@
 ;; L -> num : Procedimiento que devuelve la longitud de una lista.
 (define my-length
   (lambda (lst)
-    (if (null? lst)  
-        0  
+    (if (null? lst)
+        0
         (+ 1 (my-length (cdr lst))))))
 
 ;; Definición de Operar-binarias
 (define Operar-binarias
   (lambda (operacionB)
     (cond
-      ((number? operacionB) operacionB)  
+      ((number? operacionB) operacionB)
       ((and (list? operacionB) (= (my-length operacionB) 3))
-       (let ((izq (Operar-binarias (car operacionB)))  
-             (op (cadr operacionB))  
-             (der (Operar-binarias (caddr operacionB))))  
+       (let ((izq (Operar-binarias (car operacionB)))
+             (op (cadr operacionB))
+             (der (Operar-binarias (caddr operacionB))))
          (cond
            ((eqv? op 'suma) (+ izq der))
            ((eqv? op 'resta) (- izq der))
            ((eqv? op 'multiplica) (* izq der))
-           (else 'operador-invalido))))  
-      (else 'expresion-invalida))))  
+           (else 'operador-invalido))))
+      (else 'expresion-invalida))))
 
-      ;; Pruebas
-(display (Operar-binarias 4)) (newline)  
-(display (Operar-binarias '(2 suma 9))) (newline) 
-(display (Operar-binarias '(2 resta 9))) (newline)  
-(display (Operar-binarias '(2 multiplica 9))) (newline)  
-(display (Operar-binarias '((2 multiplica 3) suma (5 resta 1)))) (newline)  
-(display (Operar-binarias '((2 multiplica (4 suma 1)) multiplica ((2 multiplica 4) resta 1)))) (newline)  
-(display (Operar-binarias '(2 divide 3)))  ; → 'operador-invalido)
+;; Pruebas
+(display (Operar-binarias 4)) (newline)
+(display (Operar-binarias '(2 suma 9))) (newline)
+(display (Operar-binarias '(2 resta 9))) (newline)
+(display (Operar-binarias '(2 multiplica 9))) (newline)
+(display (Operar-binarias '((2 multiplica 3) suma (5 resta 1)))) (newline)
+(display (Operar-binarias '((2 multiplica (4 suma 1)) multiplica ((2 multiplica 4) resta 1)))) (newline)
+(display (Operar-binarias '(2 divide 3))) (newline)  ; → 'operador-invalido)
+
+;; Punto 17 prod-scalar-matriz:
+;; Propósito:
+;; Mat x Vec -> Mat' : Procedimiento que recibe una matriz y un vector, y devuelve una nueva matriz resultante de multiplicar cada fila de la matriz por el vector.
+;;
+;; <Mat> := ()
+;;       := (<Vec> <Mat>)
+;; <Vec> := ()
+;;       := (<num> <Vec>)
+
+(define prod-scalar-matriz
+  (lambda (mat vec)
+    (if (null? mat)
+        '()
+        (cons (zip * (car mat) vec) (prod-scalar-matriz (cdr mat) vec))
+        )
+    ))
+
+;; Pruebas
+(display (prod-scalar-matriz '((1 1) (2 2)) '(2 3))) (newline) ; -> ((2 3) (4 6))
+(display (prod-scalar-matriz '((1 1) (2 2) (3 3)) '(2 3))) (newline) ; -> ((2 3) (4 6) (6 9))
