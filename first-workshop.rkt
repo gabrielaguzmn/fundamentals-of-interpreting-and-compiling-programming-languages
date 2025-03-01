7ac1141e5bc80d162160c67070ac9cbafa5bdb84
Gabriela Guzmán <136855829+gabrielaguzmn@users.noreply.github.com>
2025-03-01 10:30:31 -0500

    merge jphm into ggb

--- first-workshop.rkt
+++ first-workshop.rkt
@@ -22,7 +22,6 @@
 
 ;filter
 
-
 (define (filtrar x L)
   (cond
     [(null? L) '()]   ; Caso base: lista vacía, devuelve lista vacía
@@ -36,6 +35,7 @@
 ;; <lista> := ()
 ;;         | (<par> <lista>)
 ;; <par> := (<valor> <valor>)
+
 (define invert
   (lambda (l)
     (if (null? l)
@@ -71,6 +71,16 @@
 (display(down '((una) (buena) (idea)))) (newline) ; -> (((una)) ((buena)) ((idea)))
 (display(down '(un (objeto (mas)) complicado))) (newline) ; -> ((un) ((objeto (mas))) (complicado))
 
+;punto 3
+
+(define (list-set L n x)
+                  (cond
+                   [(null? L) '()] ;si la lista esta vacia, retorna una lista vacia
+                   [(zero? n) (cons x (cdr L))] ; si n es 0 reemplaza el primer elemento
+                   [(>= n (longi L)) 'error]  ; Si n es mayor o igual a la longitud de L, retorna 'error
+                   [else (cons (car L) (list-set (cdr L) (- n 1) x))]
+                   )
+                  )
 
 ;; Punto #4 filter-in:
 ;; Proposito:
@@ -123,6 +133,15 @@
 (display(list-index symbol? '(a (b c) 17 foo))) (newline) ; -> 0
 (display(list-index symbol? '(1 2 (a b) 3))) (newline) ; -> #f
 
+;punto 6
+
+(define (swapper E1 E2 L)
+  (cond
+    [(null? L) '()]  ; Caso base: si la lista está vacía, retorna una lista vacía
+    [(equal? (car L) E1) (cons E2 (swapper E1 E2 (cdr L)))]  ; Si el primer elemento es E1, lo cambia por E2
+    [(equal? (car L) E2) (cons E1 (swapper E1 E2 (cdr L)))]  ; Si el primer elemento es E2, lo cambia por E1
+    [else (cons (car L) (swapper E1 E2 (cdr L)))]))  ; Si no es ninguno, lo deja igual y sigue con el resto
+
 
 ;; Punto #7 cartesian-product:
 ;; Proposito:
@@ -172,12 +191,29 @@
 (display(mapping (lambda (d) (* d 3)) (list 1 2 2) (list 2 4 6))) (newline) ; -> ((2 6))
 (display(mapping (lambda (d) (* d 2)) (list 1 2 3) (list 3 9 12))) (newline) ; -> ()
 
+
+;punto 9
+
+(define (inversions L) 
+  
+  (cond
+  [(equal? (longi(filtrar (car L) L)) 0) (inversions(cdr L))]
+  [(> (longi(filtrar (car L) L)) 1) (inversions (juntarListas ((cdr L) ((longi(filtrar (car L) L))))) )]
+  
+
+  [else (car L)]
+  )  
+   
+  
+  )
+
 ;; Punto #10 up :
 ;; Proposito:
 ;; L -> L' : Procedimiento que remueve un nivel de paréntesis de la lista dada.
 ;;
 ;; <lista> := ()
 ;;          := (<valor> <lista>)
+
 (define up
   (lambda (L)
     (if (null? L)
@@ -217,6 +253,14 @@
 (display (zip + '(1 4) '(6 2))) (newline); -> (7 6)
 (display (zip * '(11 5 6) '(10 9 8))) (newline) ; -> (110 45 48)
 
+;punto 12
+
+(define (filter-acum a b F acum filter)
+  (if (> a b)  ; Caso base: si a supera b, retornar acum
+      acum
+      (filter-acum (+ a 1) b F  ; Incrementamos a
+                   (if (filter a) (F acum a) acum)  ; Aplicamos F si a cumple con filter
+                   filter)))  ; Se mantiene el filtro
 
 ;; Punto 13 operate:
 ;; Proposito:
@@ -255,6 +299,20 @@
 
 (display (path 5 '(10 (5 () ()) (15 () ())))) (newline)  ; -> '(left)
 (display (path 17 '(14 (7 () (12 () ())) (26 (20 (17 () ()) ()) (31 () ()))))) (newline) ; -> (right left left)
+
+;punto 15
+
+(define (count-odd-and-even arbol)
+  (if (null? arbol)
+      '(0 0)  ; Caso base: árbol vacío, retorna (0,0)
+      (let* ([valor (car arbol)]
+             [izq (if (and (pair? (cdr arbol)) (pair? (cadr arbol))) (cadr arbol) '())]
+             [der (if (and (pair? (cdr arbol)) (> (longi (cdr arbol)) 1) (pair? (caddr arbol))) (caddr arbol) '())]
+             [conteo-izq (count-odd-and-even izq)]
+             [conteo-der (count-odd-and-even der)]
+             [pares (+ (if (even? valor) 1 0) (car conteo-izq) (car conteo-der))]
+             [impares (+ (if (odd? valor) 1 0) (cadr conteo-izq) (cadr conteo-der))])
+        (list pares impares))))
 
 ;; Punto #16 Operar-binarias :
 ;; Proposito:
@@ -318,67 +376,9 @@
 
 ;; Pruebas
 (display (prod-scalar-matriz '((1 1) (2 2)) '(2 3))) (newline) ; -> ((2 3) (4 6))
-(display (prod-scalar-matriz '((1 1) (2 2) (3 3)) '(2 3))) (newline) ; -> ((2 3) (4 6) (6 9));punto 3
-(define (list-set L n x)
-                  (cond
-                   [(null? L) '()] ;si la lista esta vacia, retorna una lista vacia
-                   [(zero? n) (cons x (cdr L))] ; si n es 0 reemplaza el primer elemento
-                   [(>= n (longi L)) 'error]  ; Si n es mayor o igual a la longitud de L, retorna 'error
-                   [else (cons (car L) (list-set (cdr L) (- n 1) x))]
-                   )
-                  )
-;punto 6
-
-(define (swapper E1 E2 L)
-  (cond
-    [(null? L) '()]  ; Caso base: si la lista está vacía, retorna una lista vacía
-    [(equal? (car L) E1) (cons E2 (swapper E1 E2 (cdr L)))]  ; Si el primer elemento es E1, lo cambia por E2
-    [(equal? (car L) E2) (cons E1 (swapper E1 E2 (cdr L)))]  ; Si el primer elemento es E2, lo cambia por E1
-    [else (cons (car L) (swapper E1 E2 (cdr L)))]))  ; Si no es ninguno, lo deja igual y sigue con el resto
-
-;punto 9
-
-
-(define (inversions L) 
-  
-  (cond
-  [(equal? (longi(filtrar (car L) L)) 0) (inversions(cdr L))]
-  [(> (longi(filtrar (car L) L)) 1) (inversions (juntarListas ((cdr L) ((longi(filtrar (car L) L))))) )]
-  
-
-  [else (car L)]
-  )  
-   
-  
-  )
-
-
-
-  
-;punto 12
-
-(define (filter-acum a b F acum filter)
-  (if (> a b)  ; Caso base: si a supera b, retornar acum
-      acum
-      (filter-acum (+ a 1) b F  ; Incrementamos a
-                   (if (filter a) (F acum a) acum)  ; Aplicamos F si a cumple con filter
-                   filter)))  ; Se mantiene el filtro
-;punto 15
-
-(define (count-odd-and-even arbol)
-  (if (null? arbol)
-      '(0 0)  ; Caso base: árbol vacío, retorna (0,0)
-      (let* ([valor (car arbol)]
-             [izq (if (and (pair? (cdr arbol)) (pair? (cadr arbol))) (cadr arbol) '())]
-             [der (if (and (pair? (cdr arbol)) (> (longi (cdr arbol)) 1) (pair? (caddr arbol))) (caddr arbol) '())]
-             [conteo-izq (count-odd-and-even izq)]
-             [conteo-der (count-odd-and-even der)]
-             [pares (+ (if (even? valor) 1 0) (car conteo-izq) (car conteo-der))]
-             [impares (+ (if (odd? valor) 1 0) (cadr conteo-izq) (cadr conteo-der))])
-        (list pares impares))))
+(display (prod-scalar-matriz '((1 1) (2 2) (3 3)) '(2 3))) (newline) ; -> ((2 3) (4 6) (6 9))
   
 ;punto 18
-
 
 (define (sumar-consecutivos L)
   (if (or (null? L) (null? (cdr L)))
